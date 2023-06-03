cda06e0820ace35d671e4f0139b2b91e6cda8b9f
cbanupama <cbanupama03@gmail.com>
2023-06-03 13:03:11 +0530

    final push

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Docter Appointment',
+    day: 'May 31st at 6:00pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting with friends',
+    day: 'June 30th at 6:00pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shoping',
+    day: 'July 1st at 6:00pm',
+    reminder: true,
+  },
+];
